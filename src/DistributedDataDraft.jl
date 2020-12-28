a4938e2e29ea60814e5297e22e660637f25f01ba
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-12-28 10:26:43 +0100

    Starting major refactoring

--- src/DistributedDataDraft.jl
+++ src/DistributedDataDraft.jl
@@ -1,49 +1,15 @@
 module DistributedDataDraft
 
-using Gridap.Arrays: Table, length_to_ptrs!, rewind_ptrs!, lazy_map, PosNegReindex, PosNegPartition, Reindex
-using Gridap.Algebra: scale_entries!
-using SparseArrays: AbstractSparseMatrix, findnz, sparse
-using FillArrays
-using LinearAlgebra
-
-export Communicator
-export num_parts
-export num_workers
-export do_on_parts
-export map_on_parts
-export i_am_master
-export OrchestratedCommunicator
-export Communicator
+export Backend
+export distributed_run
 export DistributedData
-export get_comm
-export get_part_type
-export gather!
-export gather
-export scatter
-export bcast
-export get_distributed_data
-export assemble!
-export async_exchange!
-export exchange!
-export exchange
-export discover_parts_snd
-export num_oids
-export num_hids
-export IndexSet
-export setgid!
-export num_lids
-export Exchanger
-export allocate_rcv_buffer
-export allocate_snd_buffer
-export DistributedRange
-export num_gids
-export remove_ghost
-export DistributedVector
-export DistributedVectorSeed
-export DistributedSparseMatrix
-export AdditiveSchwarz
+export num_parts
+export map_parts
+export Part
 
-export SequentialCommunicator
+export SequentialBackend
+export sequential
+
 
 include("Helpers.jl")
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using DistributedDataDraft
 using Test
 
+include("InterfacesTests.jl")
+
+include("SequentialBackendTests.jl")
+
 @testset "DistributedDataDraft.jl" begin
     # Write your tests here.
 end
