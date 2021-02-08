7c27385887ab5d3c2991a401837a8c84ab6efdda
Francesc Verdugo <fverdugo@cimne.upc.edu>
2021-02-08 18:17:00 +0100

    Temporarily deactivating asynchronous exchanges

--- src/MPIBackend.jl
+++ src/MPIBackend.jl
@@ -193,35 +193,29 @@ function async_exchange!(
   s = parts_rcv.size
 
   t0 = t_in.part
+  wait(schedule(t0))
 
-  t1 = @async begin
+  req_all = MPI.Request[]
 
-    req_all = MPI.Request[]
-    wait(schedule(t0))
-
-    for (i,part_rcv) in enumerate(parts_rcv.part)
-      rank_rcv = part_rcv-1
-      buff_rcv = view(data_rcv.part,i:i)
-      tag_rcv = part_rcv
-      reqr = MPI.Irecv!(buff_rcv,rank_rcv,tag_rcv,comm)
-      push!(req_all,reqr)
-    end
-
-    for (i,part_snd) in enumerate(parts_snd.part)
-      rank_snd = part_snd-1
-      buff_snd = view(data_snd.part,i:i)
-      tag_snd = get_part_id(comm)
-      reqs = MPI.Isend(buff_snd,rank_snd,tag_snd,comm)
-      push!(req_all,reqs)
-    end
-
-    return req_all
+  for (i,part_rcv) in enumerate(parts_rcv.part)
+    rank_rcv = part_rcv-1
+    buff_rcv = view(data_rcv.part,i:i)
+    tag_rcv = part_rcv
+    reqr = MPI.Irecv!(buff_rcv,rank_rcv,tag_rcv,comm)
+    push!(req_all,reqr)
   end
 
-  t2 = @task begin
-    req_all = fetch(t1)
-    MPI.Waitall!(req_all)
+  for (i,part_snd) in enumerate(parts_snd.part)
+    rank_snd = part_snd-1
+    buff_snd = view(data_snd.part,i:i)
+    tag_snd = get_part_id(comm)
+    reqs = MPI.Isend(buff_snd,rank_snd,tag_snd,comm)
+    push!(req_all,reqs)
   end
+
+  MPI.Waitall!(req_all)
+
+  t2 = @task nothing
 
   t_out = MPIData(t2,comm,s)
   t_out
@@ -241,37 +235,31 @@ function async_exchange!(
   s = parts_rcv.size
 
   t0 = t_in.part
+  wait(schedule(t0))
 
-  t1 = @async begin
+  req_all = MPI.Request[]
 
-    req_all = MPI.Request[]
-    wait(schedule(t0))
-
-    for (i,part_rcv) in enumerate(parts_rcv.part)
-      rank_rcv = part_rcv-1
-      ptrs_rcv = data_rcv.part.ptrs
-      buff_rcv = view(data_rcv.part.data,ptrs_rcv[i]:(ptrs_rcv[i+1]-1))
-      tag_rcv = part_rcv
-      reqr = MPI.Irecv!(buff_rcv,rank_rcv,tag_rcv,comm)
-      push!(req_all,reqr)
-    end
-
-    for (i,part_snd) in enumerate(parts_snd.part)
-      rank_snd = part_snd-1
-      ptrs_snd = data_snd.part.ptrs
-      buff_snd = view(data_snd.part.data,ptrs_snd[i]:(ptrs_snd[i+1]-1))
-      tag_snd = get_part_id(comm)
-      reqs = MPI.Isend(buff_snd,rank_snd,tag_snd,comm)
-      push!(req_all,reqs)
-    end
-
-    return req_all
+  for (i,part_rcv) in enumerate(parts_rcv.part)
+    rank_rcv = part_rcv-1
+    ptrs_rcv = data_rcv.part.ptrs
+    buff_rcv = view(data_rcv.part.data,ptrs_rcv[i]:(ptrs_rcv[i+1]-1))
+    tag_rcv = part_rcv
+    reqr = MPI.Irecv!(buff_rcv,rank_rcv,tag_rcv,comm)
+    push!(req_all,reqr)
   end
 
-  t2 = @task begin
-    req_all = fetch(t1)
-    MPI.Waitall!(req_all)
+  for (i,part_snd) in enumerate(parts_snd.part)
+    rank_snd = part_snd-1
+    ptrs_snd = data_snd.part.ptrs
+    buff_snd = view(data_snd.part.data,ptrs_snd[i]:(ptrs_snd[i+1]-1))
+    tag_snd = get_part_id(comm)
+    reqs = MPI.Isend(buff_snd,rank_snd,tag_snd,comm)
+    push!(req_all,reqs)
   end
+
+  MPI.Waitall!(req_all)
+
+  t2 = @task nothing
 
   t_out = MPIData(t2,comm,s)
   t_out
