e6545faeb0c19b823a34ab6067fa31f353f6ee46
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2023-01-18 10:34:34 +0100

    Adding more tests for PVector

--- test/new/p_vector_tests.jl
+++ test/new/p_vector_tests.jl
@@ -66,4 +66,55 @@ function p_vector_tests(distribute)
     @test sqrt(a⋅a) ≈ norm(a)
     @test euclidean(a,a) + 1 ≈ 1
 
+    n = 10
+    parts = rank
+    indices = map(parts) do part
+        if part == 1
+            LocalIndices(n,part,[1,2,3,5,7,8],Int32[1,1,1,2,3,3])
+        elseif part == 2
+            LocalIndices(n,part,[2,4,5,10],Int32[1,2,2,4])
+        elseif part == 3
+            LocalIndices(n,part,[6,7,8,5,4,10],Int32[3,3,3,2,2,4])
+        else
+            LocalIndices(n,part,[1,3,7,9,10],Int32[1,1,3,4,4])
+        end
+    end
+    rows = PRange(n,indices)
+    v = pzeros(rows)
+    map(parts,v.values,v.rows.indices) do part, values, indices
+        local_to_owner = get_local_to_owner(indices)
+        for lid in 1:length(local_to_owner)
+            owner = local_to_owner[lid]
+            if owner == part
+                values[lid] = 10*part
+            end
+        end
+    end
+    consistent!(v) |> wait
+
+    map(v.values,v.rows.indices) do values, indices
+        local_to_owner = get_local_to_owner(indices)
+        for lid in 1:length(local_to_owner)
+            owner = local_to_owner[lid]
+            @test values[lid] == 10*owner
+        end
+    end
+
+    map(get_local_values(v)) do values
+        fill!(values,10.0)
+    end
+
+    assemble!(v) |> wait
+    map(parts,get_local_values(v)) do part,values
+        if part == 1
+            @test values == [20.0, 20.0, 20.0, 0.0, 0.0, 0.0]
+        elseif part == 2
+            @test values == [0.0, 20.0, 30.0, 0.0]
+        elseif part == 3
+            @test values == [10.0, 30.0, 20.0, 0.0, 0.0, 0.0]
+        else
+            @test values == [0.0, 0.0, 0.0, 10.0, 30.0]
+        end
+    end
+
 end
