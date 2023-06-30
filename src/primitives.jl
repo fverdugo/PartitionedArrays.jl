2442cd1d0bf518236803433a82f46db1304b99d1
Alberto F. Martin <alberto.martin@monash.edu>
2023-06-30 19:12:34 +1000

    Move helper fun _is_included within ExchangeGraph_impl

--- src/primitives.jl
+++ src/primitives.jl
@@ -583,21 +583,20 @@ function ExchangeGraph(snd;neighbors=nothing,symmetric=false)
     end
 end
 
-function _is_included(snd_ids_a,snd_ids_b)
-    is_included_all=map(snd_ids_a, snd_ids_b) do snd_ids_a, snd_ids_b
-        all(i->i in snd_ids_b,snd_ids_a)
-    end
-    result=false
-    and(a,b)=a && b
-    is_included_all=reduction(and,is_included_all,destination=:all,init=one(eltype(is_included_all)))
-    map(is_included_all) do is_included_all
-        result = is_included_all
-    end
-    result
-end 
-
 # Discover snd parts from rcv assuming that snd is a subset of neighbors
 function ExchangeGraph_impl(snd_ids,neighbors::ExchangeGraph)
+    function _is_included(snd_ids_a,snd_ids_b)
+        is_included_all=map(snd_ids_a, snd_ids_b) do snd_ids_a, snd_ids_b
+            all(i->i in snd_ids_b,snd_ids_a)
+        end
+        result=false
+        and(a,b)=a && b
+        is_included_all=reduction(and,is_included_all,destination=:all,init=one(eltype(is_included_all)))
+        map(is_included_all) do is_included_all
+            result = is_included_all
+        end
+        result
+    end 
     @boundscheck _is_included(snd_ids,neighbors.snd) || error("snd_ids must be a subset of neighbors.snd")
     rank = linear_indices(snd_ids)
     # Tell the neighbors whether I want to send to them
