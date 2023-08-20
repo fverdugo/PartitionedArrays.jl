091ea2266ce7ea25cba45ba9d9adf10633eb36f7
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2023-08-20 16:14:04 +0200

    Some docs

--- src/p_range.jl
+++ src/p_range.jl
@@ -697,11 +697,24 @@ function variable_partition(
 end
 
 """
-    partition_from_color(ranks,global_to_owner;multicast=false,source=MAIN)
+    partition_from_color(ranks,global_to_color;multicast=false,source=MAIN)
 
-!!! warning
-    Document me! Looking for help. Open a PR.
+Build an arbitrary 1d partition by defining the parts via the argument `global_to_color`.
+The output is a vector of vectors containing the indices in each component of
+the partition. The `eltype` of the result implements the [`AbstractLocalIndices`](@ref)
+interface.
 
+# Arguments
+
+- `ranks`: Array containing the distribution of ranks.
+- `global_to_color`: If `multicast==false`,  `global_to_color[gid]` contains the part id that own the global id `gid`. If `multicast==true`, then   `global_to_color[source][gid]` contains the part id that own the global id `gid`.
+
+# Key-word arguments
+- `multicast=false`
+- `source=MAIN`
+
+This function is useful when generating a partition using a graph partitioner such as METIS.
+The argument `global_to_color` is the usual output of such tools.
 """
 function partition_from_color(ranks,global_to_color;multicast=false,source=MAIN)
     if multicast == true
