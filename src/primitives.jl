02343e3c60983b673b81345dc715cd14a511416c
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2023-12-18 18:15:26 +0100

    Adding new kw-arg in ExchangeGraph

--- src/primitives.jl
+++ src/primitives.jl
@@ -569,12 +569,16 @@ function default_find_rcv_ids(::AbstractArray)
 end 
 
 """
-    ExchangeGraph(snd; symmetric=false [,neighbors,find_rcv_ids])
+    ExchangeGraph(snd; symmetric=false [rcv, neighbors,find_rcv_ids])
 
 Create an `ExchangeGraph` object only from the lists of outgoing 
-neighbors in `snd`. If `symmetric==true`, then the incoming neighbors
+neighbors in `snd`. In case the list of incoming neighbors is
+known, it can be passed as key-word argument by setting `rcv` and the rest of
+key-word arguments are ignored.
+If `symmetric==true`, then the incoming neighbors
 are set to `snd`. Otherwise, either the optional `neighbors` or 
-`find_rcv_ids` are considered, in that order. `neighbors` is also an `ExchangeGraph`
+`find_rcv_ids` are considered, in that order. 
+`neighbors` is also an `ExchangeGraph`
 that contains a super set of the outgoing and incoming neighbors
 associated with `snd`. It is used to find the incoming neighbors `rcv`
 efficiently. If `neighbors` are not provided, then `find_rcv_ids` 
@@ -583,10 +587,13 @@ is used (either the user-provided or a default one).
 rcv side of the exchange graph out of the snd side information.
 """
 function ExchangeGraph(snd;
+                       rcv=nothing,
                        neighbors=nothing,
                        symmetric=false,
                        find_rcv_ids=default_find_rcv_ids(snd))
-    if symmetric
+    if (rcv != nothing)
+        ExchangeGraph(snd,rcv)
+    elseif symmetric
         ExchangeGraph(snd,snd)
     elseif (neighbors != nothing)
         ExchangeGraph_impl_with_neighbors(snd,neighbors)
