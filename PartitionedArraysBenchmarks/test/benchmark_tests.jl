ce0f9b4578328385010e595a23e6445c7131d458
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-02-23 12:48:53 +0100

    Testing the benchmarks also with 4 mpiranks

--- PartitionedArraysBenchmarks/test/benchmark_tests.jl
+++ PartitionedArraysBenchmarks/test/benchmark_tests.jl
@@ -2,6 +2,7 @@ module BenchmarkTests
 
 import PartitionedArraysBenchmarks as pb
 import PartitionedArrays as pa
+import MPI
 
 cells_per_dir=(10,10,10)
 parts_per_dir=(1,1,1)
@@ -26,5 +27,30 @@ out = pa.with_mpi(distribute->pb.benchmark_spmv(distribute,params,jobparams))
 params = (;nruns,cells_per_dir,parts_per_dir,method="Pestc")
 jobparams = (;)
 out = pa.with_mpi(distribute->pb.benchmark_spmv(distribute,params,jobparams))
+
+code = quote
+    import PartitionedArraysBenchmarks as pb
+    import PartitionedArrays as pa
+    pa.with_mpi() do distribute
+        cells_per_dir=(10,10,10)
+        parts_per_dir = (2,2,1)
+        jobparams = (;)
+        nruns = 10
+        params = (;nruns,cells_per_dir,parts_per_dir,method="PartitionedArrays")
+        out = pb.benchmark_spmv(distribute,params,jobparams)
+        params = (;nruns,cells_per_dir,parts_per_dir,method="Pestc")
+        out = pb.benchmark_spmv(distribute,params,jobparams)
+        params = (;nruns,cells_per_dir,parts_per_dir,method="psprse")
+        out = pb.benchmark_psparse(distribute,params,jobparams)
+        params = (;nruns,cells_per_dir,parts_per_dir,method="petsc_coo")
+        out = pb.benchmark_psparse(distribute,params,jobparams)
+        params = (;nruns,cells_per_dir,parts_per_dir,method="petsc_setvalues")
+        out = pb.benchmark_psparse(distribute,params,jobparams)
+    end
+end
+
+cmd = MPI.mpiexec()
+run(`$cmd -np 4 julia --project=PartitionedArraysBenchmarks -e $code`)
+
 
 end # module
