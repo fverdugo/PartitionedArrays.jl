75ce37e17436c725186e47058c0a240e91a72a50
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-03-22 10:22:26 +0100

    Removed analysis.jl

--- PartitionedArraysBenchmarks/test/runtests.jl
+++ PartitionedArraysBenchmarks/test/runtests.jl
@@ -5,7 +5,6 @@ using Test
 @testset "PartitionedArraysBenchmarks" begin
     @testset "benchmark_tests" begin include("benchmark_tests.jl") end
     @testset "helpers_tests" begin include("helpers_tests.jl") end
-    @testset "analysis" begin include("analysis.jl") end
 end
 
 end # module
