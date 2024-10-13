8b257f4f8715cc315501655b95a69691d7f20d62
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-10-13 12:02:00 +0200

    Deactivating old tests

--- PartitionedSolvers/test/runtests.jl
+++ PartitionedSolvers/test/runtests.jl
@@ -5,9 +5,10 @@ using PartitionedSolvers
 using Test
 
 @testset "PartitionedSolvers" begin
-    @testset "smoothers" begin include("smoothers_tests.jl") end
-    @testset "amg" begin include("amg_tests.jl") end
-    @testset "nonlinear_solvers" begin include("nonlinear_solvers_tests.jl") end
+    #@testset "smoothers" begin include("smoothers_tests.jl") end
+    #@testset "amg" begin include("amg_tests.jl") end
+    #@testset "nonlinear_solvers" begin include("nonlinear_solvers_tests.jl") end
+    @testset "linear_solvers" begin include("linear_solvers_tests.jl") end
 end
 
 end # module
