29e53281e813ca8baf9e3ee36781568415a1af5b
Francesc Verdugo <f.verdugo.rojano@vu.nl>
2024-10-13 11:57:04 +0200

    Added timings to solvers

--- PartitionedSolvers/src/linear_solvers.jl
+++ PartitionedSolvers/src/linear_solvers.jl
@@ -136,10 +136,12 @@ function LinearAlgebra_lu(A;
         verbosity=PS.verbosity(),
         timer_output=TimerOutput(),
     )
-    factors = lu(A)
-    status = Status()
-    workspace = (;factors,verbose,verbosity,timer_output,status)
-    LinearAlgebra_LU(workspace)
+    @timeit timer_output "LinearAlgebra_lu" begin
+        factors = lu(A)
+        status = Status()
+        workspace = (;factors,verbose,verbosity,timer_output,status)
+        LinearAlgebra_LU(workspace)
+    end
 end
 
 function LinearAlgebra_lu(x,p;kwargs...)
@@ -149,26 +151,30 @@ end
 
 function update!(P::LinearAlgebra_LU,p)
     (;factors,verbose,verbosity,timer_output,status) = P.workspace
-    A = matrix(p)
-    lu!(factors,A)
-    status = update(status)
-    workspace = (;factors,verbose,verbosity,timer_output,status)
-    LinearAlgebra_LU(workspace)
+    @timeit timer_output "LinearAlgebra_lu update!" begin
+        A = matrix(p)
+        lu!(factors,A)
+        status = update(status)
+        workspace = (;factors,verbose,verbosity,timer_output,status)
+        LinearAlgebra_LU(workspace)
+    end
 end
 
 uses_initial_guess(P::LinearAlgebra_LU) = false
 
 function step!(x,P::LinearAlgebra_LU,b,state=:start;kwargs...)
+    (;factors,verbose,verbosity,timer_output,status) = P.workspace
     if state === :stop
         return nothing
     end
-    (;factors,verbose,verbosity,timer_output,status) = P.workspace
-    ldiv!(x,factors,b)
-    status = step(status)
-    state = :stop
-    workspace = (;factors,verbose,verbosity,timer_output,status)
-    P = LinearAlgebra_LU(workspace)
-    x,P,state
+    @timeit timer_output "LinearAlgebra_lu step!" begin
+        ldiv!(x,factors,b)
+        status = step(status)
+        state = :stop
+        workspace = (;factors,verbose,verbosity,timer_output,status)
+        P = LinearAlgebra_LU(workspace)
+        x,P,state
+    end
 end
 
 struct JacobiCorrection{A} <: AbstractLinearSolver
@@ -179,11 +185,12 @@ function jacobi_correction(A,b;
         verbose=false,
         verbosity=PS.verbosity(),
         timer_output=TimerOutput())
-
-    Adiag = dense_diag(A)
-    status = Status()
-    workspace = (;Adiag,verbose,verbosity,timer_output,status)
-    JacobiCorrection(workspace)
+    @timeit timer_output "jacobi_correction" begin
+        Adiag = dense_diag(A)
+        status = Status()
+        workspace = (;Adiag,verbose,verbosity,timer_output,status)
+        JacobiCorrection(workspace)
+    end
 end
 
 function diagonal_solve!(x,Adiag,b)
@@ -193,57 +200,67 @@ end
 
 function update!(P::JacobiCorrection,p)
     (;Adiag,verbose,verbosity,timer_output,status) = P.workspace
-    A = matrix(p)
-    dense_diag!(Adiag,A)
-    status = update(status)
-    workspace = (;Adiag,verbose,verbosity,timer_output,status)
-    JacobiCorrection(workspace)
+    @timeit timer_output "jacobi_correction update!" begin
+        A = matrix(p)
+        dense_diag!(Adiag,A)
+        status = update(status)
+        workspace = (;Adiag,verbose,verbosity,timer_output,status)
+        JacobiCorrection(workspace)
+    end
 end
 
 function step!(x,P::JacobiCorrection,b,state=:start;kwargs...)
+    (;Adiag,verbose,verbosity,timer_output,status) = P.workspace
     if state === :stop
         return nothing
     end
-    (;Adiag,verbose,verbosity,timer_output,status) = P.workspace
-    diagonal_solve!(x,Adiag,b)
-    status = step(status)
-    state = :stop
-    workspace = (;Adiag,verbose,verbosity,timer_output,status)
-    P = JacobiCorrection(workspace)
-    x,P,state
+    @timeit timer_output "jacobi_correction step!" begin
+        diagonal_solve!(x,Adiag,b)
+        status = step(status)
+        state = :stop
+        workspace = (;Adiag,verbose,verbosity,timer_output,status)
+        P = JacobiCorrection(workspace)
+        x,P,state
+    end
 end
 
 struct IdentitySolver{A} <: AbstractLinearSolver
     workspace::A
 end
 
-function identity_solver(
+function identity_solver(;
         verbose=false,
         verbosity=PS.verbosity(),
         timer_output=TimerOutput())
-    status = Status()
-    workspace = (;status,verbosity,verbose,timer_output)
-    IdentitySolver(workspace)
+    @timeit timer_output "identity_solver" begin
+        status = Status()
+        workspace = (;status,verbosity,verbose,timer_output)
+        IdentitySolver(workspace)
+    end
 end
 
 function update!(P::IdentitySolver,p)
     (;status,verbosity,verbose,timer_output) = P.workspace
-    status = update(status)
-    workspace = (;status,verbosity,verbose,timer_output)
-    IdentitySolver(workspace)
+    @timeit timer_output "identity_solver update!" begin
+        status = update(status)
+        workspace = (;status,verbosity,verbose,timer_output)
+        IdentitySolver(workspace)
+    end
 end
 
 function step!(x,P::IdentitySolver,b,state=:start;kwargs...)
-    if state === :stop
-        return nothing
-    end
     (;status,verbosity,verbose,timer_output) = P.workspace
-    copy!(x,b)
-    status = step(status)
-    state = :stop
-    workspace = (;status,verbosity,verbose,timer_output)
-    P = IdentitySolver(workspace)
-    x,P,state
+        if state === :stop
+            return nothing
+        end
+    @timeit timer_output "identity_solver step!" begin
+        copy!(x,b)
+        status = step(status)
+        state = :stop
+        workspace = (;status,verbosity,verbose,timer_output)
+        P = IdentitySolver(workspace)
+        x,P,state
+    end
 end
 
 struct Richardson{A} <: AbstractLinearSolver
@@ -253,55 +270,71 @@ end
 function richardson(x,A,b;
         omega = 1,
         iterations = 10,
-        P=identity_solver(),
+        preconditioner=identity_solver(),
         verbose=false,
         verbosity=PS.verbosity(),
         timer_output=TimerOutput()
     )
-    r = similar(b)
-    dx = similar(x,axes(A,2))
-    status = Status()
-    target = 0
-    convergence = Convergence(iterations,target)
-    workspace = (;A,r,dx,P,status,convergence,omega,verbose,verbosity,timer_output)
-    Richardson(workspace)
+    @timeit timer_output "richardson" begin
+        P = preconditioner
+        r = similar(b)
+        dx = similar(x,axes(A,2))
+        status = Status()
+        target = 0
+        convergence = Convergence(iterations,target)
+        workspace = (;A,r,dx,P,status,convergence,omega,verbose,verbosity,timer_output)
+        Richardson(workspace)
+    end
 end
 
 function update!(S::Richardson,p)
     (;A,r,dx,P,status,convergence,omega,verbose,verbosity,timer_output) = S.workspace
-    P = update!(P,p)
-    status = update(status)
-    workspace = (;A,r,dx,P,status,convergence,omega,verbose,verbosity,timer_output)
-    Richardson(workspace)
+    @timeit timer_output "richardson update!" begin
+        P = update!(P,p)
+        status = update(status)
+        workspace = (;A,r,dx,P,status,convergence,omega,verbose,verbosity,timer_output)
+        Richardson(workspace)
+    end
 end
 
 function step!(x,S::Richardson,b,phase=:start;kwargs...)
-    @assert phase in (:start,:stop,:advance)
-    if phase === :stop
-        display(S.workspace.convergence)
-        return nothing
-    end
     (;A,r,dx,P,status,convergence,omega,verbose,verbosity,timer_output) = S.workspace
-    if phase === :start
-        current = convergence.iterations
-        convergence = start(convergence,current)
-        print_progress(convergence,verbosity)
-        phase = :advance
+    @timeit timer_output "richardson step!" begin
+        @assert phase in (:start,:stop,:advance)
+        if phase === :stop
+            verbose && display(convergence)
+            return nothing
+        end
+        if phase === :start
+            current = convergence.iterations
+            convergence = start(convergence,current)
+            verbose && print_progress(convergence,verbosity)
+            phase = :advance
+        end
+        dx .= x
+        @timeit timer_output "richardson step! mul!" begin
+            mul!(r,A,dx)
+        end
+        r .-= b
+        @timeit timer_output "richardson step! ldiv!" begin
+            ldiv!(dx,P,r)
+        end
+        x .-= omega .* dx
+        current = convergence.current - 1
+        convergence = step(convergence,current)
+        status = step(status)
+        verbose && print_progress(convergence,verbosity)
+        if converged(convergence)
+            phase = :stop
+        end
+        workspace = (;A,r,dx,P,status,convergence,omega,verbose,verbosity,timer_output)
+        S = Richardson(workspace)
+        x,S,phase
     end
-    dx .= x
-    mul!(r,A,dx)
-    r .-= b
-    ldiv!(dx,P,r)
-    x .-= omega .* dx
-    current = convergence.current - 1
-    convergence = step(convergence,current)
-    status = step(status)
-    print_progress(convergence,verbosity)
-    if converged(convergence)
-        phase = :stop
-    end
-    workspace = (;A,r,dx,P,status,convergence,omega,verbose,verbosity,timer_output)
-    S = Richardson(workspace)
-    x,S,phase
+end
+
+function jacobi(x,A,b;timer_output=TimerOutput(),kwargs...)
+    preconditioner = jacobi_correction(A,b;timer_output,kwargs...)
+    richardson(x,A,b;timer_output,preconditioner,kwargs...)
 end
 
--- PartitionedSolvers/test/linear_solvers_tests.jl
+++ PartitionedSolvers/test/linear_solvers_tests.jl
@@ -2,17 +2,22 @@ module LinearSolversTests
 
 using PartitionedArrays
 import PartitionedSolvers as PS
+using TimerOutputs
 
 args = laplacian_fem((10,10,10))
 A = sparse_matrix(args...)
 x = ones(axes(A,2))
 b = A*x
 
+timer_output = TimerOutput()
+
 P = PS.LinearAlgebra_lu(A,verbose=true,verbosity=(;indentation=">>>> "))
 
-#P = PS.identity_solver()
+P = PS.identity_solver(;timer_output)
 
-#P = PS.richardson(x,A,b)
+P = PS.richardson(x,A,b;preconditioner=P,timer_output)
+
+P = PS.jacobi(x,A,b;timer_output)
 
 x,P = PS.solve!(x,P,b)
 x,P = PS.solve!(x,P,b)
@@ -20,7 +25,7 @@ P = PS.update!(P,A)
 x, P =PS.solve!(x,P,b)
 
 P |> PS.status |> display
-#P |> PS.timer_output |> display
+P |> PS.timer_output |> display
 
 
 
